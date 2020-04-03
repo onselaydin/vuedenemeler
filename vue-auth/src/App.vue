012bf6d26d89f561aeb67ed532b139e1e024e1a8
Önsel AYDIN <onselaydin@gmail.com>
2020-04-03 10:39:15 +0300

    vue auth,login,localStorage

---
+++ vue-auth/src/App.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <Header />
+      <!-- <HomePage /> -->
+      <router-view></router-view>
+  </div>
+</template>
+<script>
+  import Header from "./components/Header";
+  
+  export default {
+    components: {
+        Header
+    },
+    created(){
+      this.$store.dispatch("initAuth")
+    }
+  }
+</script>
